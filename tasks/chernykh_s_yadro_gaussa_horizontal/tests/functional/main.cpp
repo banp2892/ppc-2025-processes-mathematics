fab47535edc9553a4448118fd3bb4b056f5b87b0
banp2892 <sseva2892@gmail.com>
2025-12-24 19:16:09 +0300

    поменял функ тесты + добавил ручной, сланг прошел, походу конiц

--- tasks/chernykh_s_yadro_gaussa_horizontal/tests/functional/main.cpp
+++ tasks/chernykh_s_yadro_gaussa_horizontal/tests/functional/main.cpp
@@ -58,43 +58,26 @@ class ChernykhSRunFuncTestsGaussaHorizontal : public ppc::util::BaseRunFuncTests
     ReadImage("chigur.jpg", size);
   }
   bool CheckTestOutputData(OutType &output_data) final {
-    int rank = 0;
-    int is_mpi_active = 0;
-    MPI_Initialized(&is_mpi_active);
-    if (is_mpi_active != 0) {
-      MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    }
-    if (rank != 0) {
-      return true;
-    }
-    int w = std::get<0>(input_data_);
-    int h = std::get<1>(input_data_);
-    const std::vector<int> &input_pixels = std::get<2>(input_data_);
-    std::vector<int> reference_output(static_cast<size_t>(w) * static_cast<size_t>(h));
-    for (int i = 0; i < h; ++i) {
-      for (int j = 0; j < w; ++j) {
-        int sum = 0;
-        for (int ki = -1; ki <= 1; ++ki) {
-          for (int kj = -1; kj <= 1; ++kj) {
-            int stroka = std::clamp(i + ki, 0, h - 1);
-            int stolbec = std::clamp(j + kj, 0, w - 1);
-            int weight = 0;
-            if (ki == 0 && kj == 0) {
-              weight = 4;
-            } else if (ki == 0 || kj == 0) {
-              weight = 2;
-            } else {
-              weight = 1;
-            }
-            sum += input_pixels[(stroka * w) + stolbec] * weight;
-          }
-        }
-        reference_output[(i * w) + j] = sum / 16;
-      }
-    }
-
-    return output_data == reference_output;
+  int rank = 0;
+  int is_mpi_active = 0;
+  MPI_Initialized(&is_mpi_active);
+  if (is_mpi_active != 0) {
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   }
+
+  if (rank != 0) {
+    return true;
+  }
+  ChernykhSYadroGaussaHorizontalSEQ task_seq(input_data_);
+  if (task_seq.Validation()) {
+    task_seq.PreProcessing();
+    task_seq.Run();
+    task_seq.PostProcessing();
+  } else {
+    return false;
+  }
+  return output_data == task_seq.GetOutput();
+}
 
  private:
   InType input_data_;
@@ -158,7 +141,7 @@ const auto kGtestValues = ppc::util::ExpandToValues(kTestTasksList);
 const auto kPerfTestName =
     ChernykhSRunFuncTestsGaussaHorizontal::PrintFuncTestName<ChernykhSRunFuncTestsGaussaHorizontal>;
 
-INSTANTIATE_TEST_SUITE_P(HypercubeTests, ChernykhSRunFuncTestsGaussaHorizontal, kGtestValues, kPerfTestName);
+INSTANTIATE_TEST_SUITE_P(YadroGaussaHorizontalTest, ChernykhSRunFuncTestsGaussaHorizontal, kGtestValues, kPerfTestName);
 
 }  // namespace
 }  // namespace chernykh_s_yadro_gaussa_horizontal
